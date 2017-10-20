72491beaeb5221feb752458e1d74486ce2dc3a78
aaaaan <aaansieun@gmail.com>
2017-10-20 11:08:56 +0900

    first commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,18 @@
+
+public class MyFirstClass {
+	public static void main(String[] args) {
+		//데이터타입
+		int i = 1;
+		short s = 1;
+		byte b = 1;
+		long l = 1;
+		
+		float f = 1.1F;
+		double d = 1.1;
+		
+		boolean bool = true;
+		char c = '강';
+		
+		System.out.println("");
+	} 
+}
